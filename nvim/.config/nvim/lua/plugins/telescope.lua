6dc9f80ee3f47e63ed7c7b4c518f6a7182100216
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-02-06 17:23:44 -0300

    Search hidden files with telescope

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+-- if true then return {} end
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    pickers = {
+      live_grep = {
+        file_ignore_patterns = { "node_modules", ".git", ".venv" },
+        additional_args = function(_)
+          return { "--hidden" }
+        end,
+      },
+      find_files = {
+        file_ignore_patterns = { "node_modules", ".git", ".venv" },
+        hidden = true,
+      },
+    },
+  },
+}
