024b21d31435b22bb3ec9b1bd4e4da37dfca0a5f
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-09-07 05:31:11 -0300

    Disable proto in clangd config for nvim

---
+++ nvim/.config/nvim/lua/plugins/clangd.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- removes `proto` from the default list
+        clangd = { filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "hpp" } },
+      },
+    },
+  },
+}
